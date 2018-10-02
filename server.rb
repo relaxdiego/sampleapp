9199d52455a39114faf02cb993680907fdb5f31b
Mark Maglana <mmaglana@tesla.com>
2018-10-02 21:22:09 +0800

    Change to json

--- server.rb
+++ server.rb
@@ -1,25 +1,23 @@
 #!/usr/local/bin/ruby
 
 require 'sinatra'
+require 'json'
 
 set :bind, '0.0.0.0'
 
 get '/' do
-  @version = 1
+  content_type 'application/json'
 
-  <<-EOF
-  <html>
-    <head>
-      <title>Kubernetes Demo</title>
-    </head>
-    <body>
-      <div>
-        <p>Version: #{@version}</p>
-        <p>Pod name: #{ENV["MY_POD_NAME"]}</p>
-        <p>Pod IP: #{ENV["MY_POD_IP"]}</p>
-        <p>Node: #{ENV["MY_NODE_NAME"]}</p>
-      </div>
-    </body>
-  </html>
-  EOF
+  @version = 2
+  @@hits ||= 0
+
+  @@hits += 1
+
+  {
+    version: @version,
+    pod_name: ENV['MY_POD_NAME'],
+    pod_ip: ENV['MY_POD_IP'],
+    node_name: ENV['MY_NODE_NAME'],
+    hits: @@hits
+  }.to_json
 end
